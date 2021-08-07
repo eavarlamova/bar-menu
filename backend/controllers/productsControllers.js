dc9e2a6db556d6751936b00aaa979c261803dd59
Ekaterina Varlamova <e.varlamova@dunice.net>
2021-08-08 01:16:19 +0300

    simple adding new  product

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -1,26 +1,27 @@
 const { Products, Users } = require('../models')
 
+
+
+// const allProducts = await Products.findAll({
+//     include: [{
+//         model: Users,
+//         as: 'author',
+//         attributes: ['email', 'name', 'avatar']
+//     }],
+// })
+
+
+
 const productsControllers = {
     async add({ body }, res, next) {
         try {
             // validate body
-            // const newProduct = await Products.create(body)
-
-            const allProducts = await Products.findAll({
-                include: [{
-                    model: Users,
-                    as: 'author',
-                    attributes: ['email', 'name', 'avatar']
-                }],
-            })
-            console.log('allProducts', allProducts)
-            // console.log('newProduct', newProduct)
-            res.status(200).send(allProducts)
+            const newProduct = await Products.create(body)
+            res.status(200).send(newProduct)
         }
         catch (error) {
-            console.log('#######', error, '#######')
+            res.status(500).send({ msg: 'oops... some problem with adding your product' })
         }
-
     },
 }
 
